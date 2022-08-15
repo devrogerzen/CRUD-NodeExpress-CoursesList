ebb5581aea2982ea1cfc43cb425c248b99d9335c
devrogerzen <devrogerzen@gmail.com>
2022-08-15 18:52:07 -0300

    Crud list courses acedemy and use routing and no db

---
+++ App.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const { infoCursos } = require("./datos/cursos");
+
+//Routers
+const routerProgramacion = require("./routers/programacion");
+app.use("/api/cursos/programacion", routerProgramacion);
+
+const routerMatematicas = require("./routers/matematicas");
+app.use("/api/cursos/matematicas", routerMatematicas);
+
+//Routing
+app.get("/", (req, res) => {
+  res.send("Mi primer servidor. Cursosss 🎮");
+});
+
+app.get("/api/cursos", (req, res) => {
+  res.json(infoCursos);
+});
+
+const PUERTO = process.env.PORT || 3000;
+app.listen(PUERTO, () => {
+  console.log(`El servidor esta escuchando en el puerto ${PUERTO} ...`);
+});
